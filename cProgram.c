3c23ef709ae8fe40c9cc03f78eb3eaf993ab5adc
Student <student@coms327-vm.cs.iastate.edu>
2019-09-25 19:32:47 -0500

    Import files to repository

---
+++ cProgram.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int printArray();
+bool swap();
+
+int main() {
+  printf("Hello World\n");
+
+  int a[10];
+
+  int sizeA = sizeof(a)/sizeof(int);
+  for (size_t i = 0; i < sizeA; i++) {
+    /* code */
+    a[i] = 1; //random();
+  }
+
+  printArray(a, sizeA);
+
+  return 0;
+}
+
+int printArray(int *array, int length) {
+  for (size_t i = 0; i < length; i++) {
+    /* code */
+    printf("Value of array[%d] is : %d \n", i, *array);
+    array++;
+  }
+  return 1;
+}
+
+bool swap() {
+
+}
